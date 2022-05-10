27b2f9ec9781021021fd9ecae47870b4100a365a
Mateus Henrique <mateush.study@gmail.com>
2022-05-10 20:15:36 -0300

    Fix text formatting

--- SingleResponsibilityPrinciple/src/main/java/tutorial/Main.java
+++ SingleResponsibilityPrinciple/src/main/java/tutorial/Main.java
@@ -5,28 +5,28 @@ package tutorial;
      This represents the first principle in tbe acronym SOLID.
 
     What this principle tells us
-     THERE SHOULD NEVER BE MORE THAN ONE REASON FOR A CLASS TOP CHANGE.
+    THERE SHOULD NEVER BE MORE THAN ONE REASON FOR A CLASS TOP CHANGE.
 
     Meaning
-      Since we are working around the Single Responsibility principle, it means that
-     a class has to:
+    Since we are working around the Single Responsibility principle, it means that
+    a class has to:
         • Be focused.
         • Single functionality.
         • Addresses a specific concern.
 
     Example where this principle should be applied to
     • One example brought up by the video, it when a single class is responsible to
-     send a message to a server, in other words, all the necessary code to connect to
-     the server, set the message up is handled by a single class.
+      send a message to a server, in other words, all the necessary code to connect to
+      the server, set the message up is handled by a single class.
     • When analysing this, we can notice that we would have plenty of reasons to
-     modify this class, reasons such as: changing the file format (JSON -> XML), updating
-     an HTTP connection, server authentication, etc. These are some of the many reasons why this pattern should be
-     applied.
+      modify this class, reasons such as: changing the file format (JSON -> XML), updating
+      an HTTP connection, server authentication, etc. These are some of the many reasons why this pattern should be
+      applied.
 
     In conclusion
-    According to the number of responsibilities that we have, we have to have the same number of classes in order to
-    keep the multiple responsibilities separated from each other.
-    This is important for keeping the code maintainable for future changes.
+    • According to the number of responsibilities that we have, we have to have the same number of classes in order to
+      keep the multiple responsibilities separated from each other.
+    • This is important for keeping the code maintainable for future changes.
  */
 
 import java.io.IOException;
